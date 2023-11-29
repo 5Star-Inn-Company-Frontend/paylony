11d7d400866d6e2b9a2cba180770ef775272cdff
hamskid-id <lawalhamzat27@gmail.com>
2023-11-29 13:28:34 -0800

    updated

--- src/components/auth/authLayout.jsx
+++ src/components/auth/authLayout.jsx
@@ -2,7 +2,7 @@ import logo from "../../assets/Paylony logo 1.png"
 
 export const AuthLayout =({children,adjHeight,style})=>{
     return(
-        <div className={`${adjHeight?"ht-100":" h-screen"} authLayout py-8 px-4 flex justify-center items-center `}>
+        <div className={`${adjHeight?"ht-100":" h-screen"} authLayout py-8 lg:px-4 xl:px-4 md:px-4 sm:px-2 xs:px-2 xxs:px-2 xxxs:px-2 flex justify-center items-center `}>
             <div 
                 className={`${style? style:"mx-auto lg:w-2/6 xl:w-2/6 md:w-2/6 sm:w-full h-[100%] xs:w-full xxs:w-full"} p-3`}
             >
@@ -13,7 +13,7 @@ export const AuthLayout =({children,adjHeight,style})=>{
                             className="w-18 h-18"
                         />
                     </div>
-                <div className="overflow-auto bg-white rounded-lg lg:px-4 xl:px-4 md:px-4 sm:px-2 xs:px-2 xxs:px-2 xxxs:px-2 py-8 ">
+                <div className="overflow-auto bg-white rounded-lg px-4 py-8 ">
                     {children}
                 </div>
             </div>
